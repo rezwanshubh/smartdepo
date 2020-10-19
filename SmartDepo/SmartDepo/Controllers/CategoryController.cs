111cc81d18f19319e35c7131ce2737bf1ed5a2e5
Rezwanul Islam <rezwanul.islam@arvato.com>
2020-10-19 22:09:16 +0300

    Add references

---
+++ SmartDepo/SmartDepo/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SmartDepo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpPost]
+        [Route("save")]
+        public void Insert()
+        {
+            
+        }
+    }
+}
