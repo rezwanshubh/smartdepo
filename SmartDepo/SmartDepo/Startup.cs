138cb472c538bbff5d8b09dcd9571b3c86f5957e
Rezwanul Islam <rezwanul.islam@arvato.com>
2020-10-24 20:08:57 +0300

    Create database if not exist

--- SmartDepo/SmartDepo/Startup.cs
+++ SmartDepo/SmartDepo/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SmartDepo.Data;
 
 namespace SmartDepo
 {
@@ -27,6 +29,10 @@ namespace SmartDepo
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            // db connection
+            services.AddDbContext<SmartDepoContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen();
         }
@@ -34,6 +40,13 @@ namespace SmartDepo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // create db
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<SmartDepoContext>();
+                context.Database.EnsureCreated();
+            }
+
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger(c =>
             {
